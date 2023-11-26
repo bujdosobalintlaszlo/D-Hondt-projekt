01ffa60d7b76f03e0d777a962ddcd80a9e7e2053
Balint <118969351+bujdosobalintlaszlo@users.noreply.github.com>
2023-11-26 20:35:06 +0100

    Alap beolvasas
    Classok: Part, Partok, Szamol(ures)

---
+++ Dhondt/Dhondt/Part.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dhondt
+{
+    /// <summary>
+    /// A pártot reprezentáló osztály, tárolja a párt nevét, nemzetét és szavazatszámát.
+    /// </summary>
+    public class Part
+    {
+        public string PartNev { get; set; }
+        public int Nemzete { get; set; }
+        public int SzavazatSzam { get; set; }
+
+        public Part(string partNev, int nemzete, int szavazatSzam)
+        {
+            PartNev = partNev;
+            Nemzete = nemzete;
+            SzavazatSzam = szavazatSzam;
+        }
+    }
+}
